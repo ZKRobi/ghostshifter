2ebd300fe517547606c30e0dca10541090327166
Zöldi-Kovács Róbert <zkovacs.robert@gmail.com>
2016-04-16 15:50:30 +0200

    sample level item level item scripts

--- Unity/Ghostshifter/Assets/Scripts/LevelGenerator.cs
+++ Unity/Ghostshifter/Assets/Scripts/LevelGenerator.cs
@@ -1,15 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LevelGenerator : MonoBehaviour {
+public class LevelGenerator : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public GameObject[] levelItems;
+
+    public float levelItemWidth;
+
+    public Transform player;
+
+    private float lastGenerated = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.position.x - lastGenerated > levelItemWidth)
+        {
+            var nextItem = (int)Mathf.Floor(levelItems.Length * Random.value);
+            if (nextItem >= levelItems.Length)
+            {
+                nextItem = levelItems.Length - 1;
+            }
+
+            lastGenerated += levelItemWidth;
+
+            GameObject newItem = Instantiate(levelItems[nextItem]);
+            newItem.transform.position = new Vector3(lastGenerated + (levelItemWidth * 2), 0, 0);
+        }
+    }
 }
---
+++ Unity/Ghostshifter/Assets/Scripts/LevelItem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelItem : MonoBehaviour
+{
+    public float width;
+    private GameObject referenceObject;
+
+    public void Start()
+    {
+        referenceObject = GameObject.FindWithTag("Player");
+    }
+
+    public void Update()
+    {
+        if (transform.position.x < referenceObject.transform.position.x - width * 2)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
