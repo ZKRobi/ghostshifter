e4cb49a1988632b2d6e9ffe3eb9064c8be4918c5
Zöldi-Kovács Róbert <zkovacs.robert@gmail.com>
2016-04-17 09:17:23 +0200

    menu, etc

---
+++ Unity/Ghostshifter/Assets/Scripts/Menace.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Menace : MonoBehaviour
+{
+
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+}
---
+++ Unity/Ghostshifter/Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler : MonoBehaviour
+{
+    public void OnStartClick()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+}
