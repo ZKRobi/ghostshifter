d201f98fa8c2110b6d71e5c059345d78fbb6a6ae
Zöldi-Kovács Róbert <zkovacs.robert@gmail.com>
2016-04-19 00:10:17 +0200

    sound, tweaking

--- Unity/Ghostshifter/Assets/Scripts/Menace.cs
+++ Unity/Ghostshifter/Assets/Scripts/Menace.cs
@@ -8,16 +8,25 @@ public class Menace : MonoBehaviour
     public float maxDistance;
     private GameObject player;
 
+    //acceleration in unity unit / second
+    public float acceleration;
+
+    new private Rigidbody2D rigidbody;
+
     // Use this for initialization
     void Start()
     {
-        GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.velocity = new Vector2(speed, 0);
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        float newSpeed = rigidbody.velocity.x + acceleration * Time.deltaTime;
+        rigidbody.velocity = new Vector2(newSpeed, 0);
+
         if (player.transform.position.x > transform.position.x + maxDistance)
         {
             transform.position = new Vector3(player.transform.position.x - maxDistance, transform.position.y, transform.position.z);
